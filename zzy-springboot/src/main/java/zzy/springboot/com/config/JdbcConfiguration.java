533444af87cec5fb87f0f0059bd7148dda044aea
zzy <zzz946041754@163.com>
2019-11-20 19:29:36 +0800

    111

--- zzy-springboot/src/main/java/zzy/springboot/com/config/JdbcConfiguration.java
+++ zzy-springboot/src/main/java/zzy/springboot/com/config/JdbcConfiguration.java
@@ -1,7 +1,10 @@
 package zzy.springboot.com.config;
 
 import com.alibaba.druid.pool.DruidDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -9,23 +12,28 @@ import org.springframework.context.annotation.PropertySource;
 import javax.sql.DataSource;
 
 @Configuration //声明一个类为java配置类，相当于一个xml文件
-@PropertySource("classpath:jdbc.properties")
+//@PropertySource("classpath:jdbc.properties") //读取资源文件
+@EnableConfigurationProperties(JdbcProperties.class)  //启用属性读取类
 public class JdbcConfiguration {
-    @Value("${jdbc.driverClassName}")
-    private String driverClassName;
-    @Value("${jdbc.url}")
-    private String url;
-    @Value("${jdbc.username}")
-    private String username;
-    @Value("${jdbc.password}")
-    private String password;
+    //属性注入
+    //@Autowired
+   // private JdbcProperties jdbcProperties;
+    /*//构造函数注入
+    public JdbcConfiguration(JdbcProperties jdbcProperties){
+        this.jdbcProperties=jdbcProperties;
+    }*/
     @Bean //把方法的返回值注入到Spring容器中
-    public DataSource dataSource(){
+    @ConfigurationProperties(prefix = "jdbc")
+    public DataSource dataSource( ){
         DruidDataSource dataSource = new DruidDataSource();
-        dataSource.setDriverClassName(driverClassName);
-        dataSource.setUrl(url);
-        dataSource.setUsername(username);
-        dataSource.setPassword(password);
-      return dataSource;
+        return dataSource;
     }
+   /* public DataSource dataSource(JdbcProperties jdbcProperties){
+        DruidDataSource dataSource = new DruidDataSource();
+        dataSource.setDriverClassName(jdbcProperties.getDriverClassName());
+        dataSource.setUrl(jdbcProperties.getUrl());
+        dataSource.setUsername(jdbcProperties.getUsername());
+        dataSource.setPassword(jdbcProperties.getPassword());
+      return dataSource;
+    }*/
 }
